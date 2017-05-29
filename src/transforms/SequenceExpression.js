d7e1f365bfa4f3bac8f69993bb9cc3b3157c9f1c
Karim Alibhai <karim@alibhai.co>
2017-05-28 23:54:55 -0400

    Precompile all source files instead of using require hook

---
+++ src/transforms/SequenceExpression.js
@@ -0,0 +1,20 @@
+/**
+ * lib/transforms/NewExpression.js - babel-preset-mjs
+ * 
+ * Licensed under MIT license.
+ * Copyright (C) 2017 Karim Alibhai.
+ */
+
+const template = require('babel-template')(`[ EXPRESSIONS ].pop()`)
+
+export function exit(path, _, t) {
+  // fix sequence expressions in by turning them
+  // into an array of expression results and popping
+  // the last one off
+  
+  path.replaceWith(
+    template({
+      EXPRESSIONS: path.node.expressions
+    })
+  )
+}
